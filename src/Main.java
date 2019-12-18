fea9362822e2f6a92b63bf46a8ceeb1df80e7e38
Cromag <alseko_2001@mail.ru>
2019-12-18 23:38:13 +0300

    MyArrayList Sort Time test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,30 @@
-public class Main {
+import java.util.Random;
 
+public class Main {
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        int size = 1000000;
+        int range = 1000000;
+
+        MyArrayList<Integer> list1 = new MyArrayList<>(size);
+        MyArrayList<Integer> list2 = new MyArrayList<>(size);
+        MyArrayList<Integer> list3 = new MyArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            int a = random.nextInt(range);
+            list1.add(a);
+            list2.add(a);
+            list3.add(a);
+        }
+        System.out.println("Bubble sort:\t" + checkMethodTime(list1::bubbleSort) + "ms");
+        System.out.println("Selection sort:\t" + checkMethodTime(list2::selectionSort) + "ms");
+        System.out.println("Insertion sort:\t" + checkMethodTime(list3::insertionSort) + "ms");
+    }
+
+    private static long checkMethodTime(Runnable method) {
+        long start = System.currentTimeMillis();
+        method.run();
+        return System.currentTimeMillis() - start;
+    }
 }
